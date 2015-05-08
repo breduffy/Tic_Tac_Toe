313b8a77abacaedd99e4778afb0efec4179836f2
breduffy <breanne.duffy@gmail.com>
2015-05-08 17:35:15 -0400

    added function to alernate between players X, O.

--- js/tic_tac_toe.js
+++ js/tic_tac_toe.js
@@ -3,33 +3,68 @@ $(document).ready(function(){
 
   console.log('starting game');
 
+var clickCounter = 0;
+var displayChars = ['X', '0'];
+
+
+function getCharacter(numOfClicks){
+  var index = numOfClicks % 2;
+  return displayChars[index];
+}
+
+
+
+
 //On click, every item that has a class of .box_cell will change the css hex background-color of that box
 //On click you generate the event, the event handler is the function
 $('.box_cell').on("click", function(event){
-    $(event.currentTarget).css("background-color", "purple").text('X');
+  clickCounter++;
+  var charToDisplay =getCharacter(clickCounter)
+  $(event.currentTarget).css("background-color", "purple").text(charToDisplay);
   });
 
-$('.box_cell').on("click", function(event){
-    $(event.currentTarget).css("background-color", "blue").text('O');
-  });
-
+// $('.box_cell').on("click", function(event){
+//     $(event.currentTarget).css("background-color", "blue").text('O');
+//   });
 
 
 
 
 });
 
+
+
+
 //NameSpace---put all javascript inside of this namespace?
-var TicTacToe = TicTacToe || {
+var TicTacToe = TicTacToe || {};
 
-// // Create board
-// var board = [
-//   [0, 0, 0],
-//   [0, 0, 0],
-//   [0, 0, 0]
-// ];
 
+// Create board
+var board = [
+  [0, 0, 0],
+  [0, 0, 0],
+  [0, 0, 0]
+];
+
+// Create variable to store the winning player
+var winningPlayer = 0;
+
+// Keep track of whose turn it is
+var turn = {
+  number : 0,
+  currentPlayerColor : function() {
+    if (this.number % 2 === 0) {
+      return 1;
+    }
+    else {
+      return 2;
+    }
+  },
+  changeTurn : function(){
+    this.number += 1;
+  }
 };
+
 
 
 
