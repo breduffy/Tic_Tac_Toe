f98bcc61a32a48c3946f0b46f1009cdb5671264f
breduffy <breanne.duffy@gmail.com>
2015-05-12 12:16:57 -0400

    Wrote the the function to get the winner, if there is a winner.

--- js/tic_tac_toe.js
+++ js/tic_tac_toe.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
 
 var clickCounter = 0;
 var displayChars = ['X', 'O'];
+var board = ['', '', '', '', '', '', '', '', ''];
 
 //This function takes the number of clicks and divides by 2. If it's 1, 1 % 2=1, 0 % 2 =0. Will alternate between 0 and 1.
 //Returns the index value of displayChars
@@ -16,7 +17,7 @@ function getCharacter(numOfClicks){
 
 //invokes getCharacters on the current clickCounter value.
 //Sets the player to the var charToDisplay
-$('.box_cell').on("click", function(){
+$('.box_cell').one("click", function(){
   clickCounter++;
   var charToDisplay = getCharacter(clickCounter);
   $(this).css("background-color", "purple").text(charToDisplay);
@@ -24,66 +25,63 @@ $('.box_cell').on("click", function(){
 //this adds X or O to the data of the cell that was clicked on
   $(this).attr('data-populatedCell', charToDisplay);
 
-  });
+  var attrId = $(this).attr('id');
+
+
+  // Create a global array to represent the state of the board.
+
+  board[attrId] = charToDisplay;
+
+  console.log(board);
+
+  if (getWinner(charToDisplay)) {
+      alert('The winner is ' + charToDisplay + '!');
+  } else if (isATie()) {
+      alert('It is a tie!');
+  }
+
+});
 //On click, every item that has a class of .box_cell will change the css hex background-color of that box
 //On click you generate the event, the event handler is the function
 
 
+//Checks to see if either X or O has hit all the winning combinations
+function getWinner(playerToken){
+  var winner = false;
+    if (
+      //Wins Horizontal
+      (playerToken === board[0] && playerToken === board[1] && playerToken === board [2]) ||
+      (playerToken === board[3] && playerToken === board[4] && playerToken === board [5]) ||
+      (playerToken === board[6] && playerToken === board[7] && playerToken === board [8]) ||
 
+      //Wins Vertically
+      (playerToken === board[0] && playerToken === board[3] && playerToken === board [6]) ||
+      (playerToken === board[1] && playerToken === board[4] && playerToken === board [7]) ||
+      (playerToken === board[2] && playerToken === board[5] && playerToken === board [8]) ||
+
+      //Wins Diagonally
+      (playerToken === board[0] && playerToken === board[4] && playerToken === board [8]) ||
+      (playerToken === board[2] && playerToken === board[4] && playerToken === board [6])) {
+
+        winner = true;
+      // console.log(winner 'is the winner');
+    }
+    return winner;
+};
+
+//Checks to see if there is a tie
+function isATie(){
+  var tie = false;
+  if (clickCounter >= 10 && getWinner == false){
+    tie = true;
+    }
+    return tie;
+  };
 
 });//End of document ready
 
 
 
 
-// // //NameSpace---put all javascript inside of this namespace?
-// // // var TicTacToe = TicTacToe || {};
-
-
-// // //How do I associate this board with the html?
-// // // Create a global array to represent the state of the board.
-// var board = ['', '', '', '', '', '', '', '', ''];
-
-
-
-
-// //Checks to see if either X or O has hit all the winning combinations
-// function getWinner(){
-//   for (i = 0; i < board.length; i++) {
-//     if ((displayChars[index] === board[0] && displayChars[index] === board[1] && displayChars[index] === board [2]) ||
-//         (displayChars[0] === board[3] && displayChars[index] === board[4] && displayChars[index] === board [5]) ||
-//         (displayChars[0] === board[6] && displayChars[index] === board[7] && displayChars[index] === board [8]) ||
-//         (displayChars[0] === board[0] && displayChars[index] === board[3] && displayChars[index] === board [6]) ||
-//         (displayChars[0] === board[1] && displayChars[index] === board[4] && displayChars[index] === board [7]) ||
-//         (displayChars[0] === board[2] && displayChars[index] === board[5] && displayChars[index] === board [8]) ||
-//         (displayChars[0] === board[0] && displayChars[index] === board[4] && displayChars[index] === board [8]))
-//     {
-//       winner = displayChars[index];
-//       console.log(winner 'is the winner');
-//     } else{
-//       winner = 'tie';
-//       console.log("It's a tie");
-//     }
-//   }
-//   return winner;
-// };
-
-
-
-
-// // //1. Set Board
-// // //2. Click X create array[]
-// // //3. Click 0 create array[]
-// // //4. FInd Winner will return winner
-// // // set a true or false to know whether they are clicked...you can set data attributes through jquery
-// // // can use toggle if isset is true, toggle does nothing, if false will be x or o
-// // //winningCombinations = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8]
-
-
-
-
-
-
-
-
-
+// NameSpace---put all javascript inside of this namespace?
+// var TicTacToe = TicTacToe || {};
